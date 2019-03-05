1aa5350dc33e11813b1bdcc557b5a5855a9f0cff
Ronald Seoh <ronaldseoh@icloud.com>
2019-03-05 14:44:14 +0900

    code cleanup

--- unmark_python_client/__init__.py
+++ unmark_python_client/__init__.py
@@ -7,29 +7,31 @@ import sys
 import warnings
 import requests
 
-class UnmarkClient(object):
+class UnmarkClient():
 
     # I'm trying to make every request to Unmark server as internal XMLHttpRequest.
     # Details in https://github.com/cdevroe/unmark/wiki/Response-Logic
     def __init__(self, server_address, email, password):
-    
+
         self.server_address = server_address
 
         self.email = email
         self.password = password
-        
+
         # Fetch CSRF Token
         self.unmark_requests_session = requests.Session()
         self._main_page_response = self.unmark_requests_session.get(self.server_address)
 
         if self._main_page_response.status_code == 200:
-            self.csrf_token = re.findall('(?:unmark.vars.csrf_token\s+=\s+\')(.+)\';', self._main_page_response.text)[0]
+            self.csrf_token = re.findall(
+                r'(?:unmark.vars.csrf_token\s+=\s+\')(.+)\';',
+                self._main_page_response.text)[0]
         else:
-            raise Exception('Unable to fetch CSRF token from your Unmark server. Status code = '+str(self._main_page_response.status_code))
+            raise Exception(
+                'Unable to fetch CSRF token from your Unmark server. '
+                'Status code = ' + str(self._main_page_response.status_code))
 
     def login(self):
-
-        self.login_url = self.server_address + '/login'
 
         login_response = self.unmark_requests_session.post(
             url=self.server_address+'/login',
@@ -44,17 +46,19 @@ class UnmarkClient(object):
                 'X-Requested-With': 'XMLHttpRequest'
             }
         )
-        
+
         if login_response.status_code != 200:
-            raise Exception('Unable to connect to the login page. Status code = '+str(self._main_page_response.status_code))
-        else:
-            # With internal XMLHttpRequest, responses are given in JSON
-            login_response_json = json.loads(login_response.text)
-            
-            if login_response_json['success'] is not True:
-                raise Exception('Login failed from the server.')
-            else:
-                return True # Success
+            raise Exception(
+                'Unable to connect to the login page. '
+                'Status code = '+str(self._main_page_response.status_code))
+
+        # With internal XMLHttpRequest, responses are given in JSON
+        login_response_json = json.loads(login_response.text)
+
+        if login_response_json['success'] is not True:
+            raise Exception('Login failed from the server.')
+
+        return True # Success
 
     def add(self, url):
         # Send a HEAD request first to url and determine the content-type
@@ -74,11 +78,11 @@ class UnmarkClient(object):
             warning_message = (
                 'Unknown Content-Type detected. '
                 'Will not attempt to get the title. '
-                'Content-Type = ' + str(content_type_string)            
+                'Content-Type = ' + str(content_type_string)
             )
-            
+
             warnings.warn(warning_message, Warning, stacklevel=2)
-            
+
             # Use url as a title
             page_title = url
         else:
@@ -88,10 +92,13 @@ class UnmarkClient(object):
             except requests.ConnectionError as e:
                 print('Unable to create a connection to the given URL. Error = ' + str(e))
                 return False
-            
+
             # Extract <title> tag
             try:
-                page_title = re.findall('(?<=<title>).+?(?=</title>)', page_response.text, re.DOTALL)[0]
+                page_title = re.findall(
+                    '(?<=<title>).+?(?=</title>)',
+                    page_response.text,
+                    re.DOTALL)[0]
             except:
                 warning_message = 'Error while extracting <title> tag.'
                 warnings.warn(warning_message, Warning, stacklevel=2)
@@ -101,7 +108,7 @@ class UnmarkClient(object):
             'url': url,
             'title': page_title
         }
-    
+
         add_response = self.unmark_requests_session.get(
             'https://unmark.serv06.iamblogger.net/mark/add',
             params=add_payload,
@@ -109,14 +116,20 @@ class UnmarkClient(object):
                 'X-Requested-With': 'XMLHttpRequest'
             }
         )
-        
+
         try:
             # With internal XMLHttpRequest, responses are given in JSON
             add_response_json = json.loads(add_response.text)
-            
-            if add_response_json['mark']['url'] == url and add_response_json['mark']['active'] == '1':
+
+            if add_response_json['mark']['url'] == url and \
+            add_response_json['mark']['active'] == '1':
                 return True # Success
-            else:
-                raise Exception("Something isn't right with the response from the server. URL = " + add_response_json['mark']['url'] + ', active = '+add_response_json['mark']['active'])
+
+            raise Exception(
+                "Something isn't right with the response from the server."
+                " URL = " + add_response_json['mark']['url'] + ", "
+                "active = "+add_response_json['mark']['active'])
         except:
-            raise Exception("Error occurred while parsing the server response. Probably means the add request have failed.")
+            raise Exception(
+                "Error occurred while parsing the server response. "
+                "Probably means the add request have failed.")
